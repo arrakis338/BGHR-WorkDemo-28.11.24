51dd71c16856af0a2fab3c312d8df97f0d4cbefd
arrakis338 <151928301+arrakis338@users.noreply.github.com>
2024-11-28 03:26:15 +0000

    build files

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Budget Guradian-1.0";
+const contentToCache = [
+    "Build/6ee980cb1bc813955ed177ea24413bd2.loader.js",
+    "Build/52978507bc48ca8a90802c7fdce7da3b.framework.js",
+    "Build/c431052549760c77b03501ee4bdfb725.data",
+    "Build/94228a890276d4da5feb7612eb57af7b.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
